830462965dcc9440001907a2ff9fefdf3bd828e4
MirruK <mirek.kudinoff@hotmail.com>
2023-04-10 13:22:42 +0300

    Minor refactoring and improvements

--- game.c
+++ game.c
@@ -45,39 +45,68 @@ void take_char(char *buf, int* buf_top){
     *buf_top += 1;
 }
 
-int validate(char *buf, int buf_top){
-    //if(buf[buf_top] == '\b') return 3;
-    if(test_text[buf_top] == '\0' && test_text[buf_top-1] == buf[buf_top-1]) return 2;
-    return test_text[buf_top-1] == buf[buf_top-1];
+int validate(char *buf, int buf_top, int text_head){
+    // if(buf[buf_top] == '\b') return 3;
+    if(test_text[text_head+1] == '\0' && test_text[text_head] == buf[buf_top-1]) return 2;
+    return test_text[text_head] == buf[buf_top-1];
 }
 
 void game_loop(int mode){
     char buf[100];
-    int result, stdin_empty;
-    int buf_top = 0;
-    clearScreen();
-    printf("\n%s\n", test_text);
+    int result, buf_top, text_head;
+    result =  text_head = 0;
+    buf_top = 1;
     if(mode == 1){
+        // Init
+        // Step 1.
+        clearScreen();
+        // Step 2.
+        printf("\n%s\r\n", test_text);
+        // Step 3.
+        empty_text[text_head] = '^';
+        printf("%s\r\n", empty_text);
+        empty_text[text_head] = ' ';
         while(1){
-            empty_text[buf_top] = '^';
-            printf("%s\r", empty_text);
-            empty_text[buf_top] = ' ';
+            /*
+             * Init Step 1. Clear screen
+             * Init Step 2. Draw text to type
+             * Init Step 3. Draw carret
+             *
+             * Step 1. take user input
+             * Step 2. Validate input
+             * Step 3. Draw according to input and start from step 1
+             * */
+
+            // Step 1.
             take_char(buf, &buf_top);
-            result = validate(buf, buf_top);
-            if(result == 0) {
-                clearScreen();
-                printf("\n%s\n", test_text);
-                printf("%s\r", empty_text);
-                printf("\r\nYou typed the wrong character: %c, try %c\r\n", buf[buf_top-1], test_text[buf_top-1]);
+            
+            clearScreen();
+            // Step 2. 
+            switch(validate(buf, buf_top, text_head)) {
+                case 0:
+                printf("\r\nYou typed the wrong character: %c, try %c\r\n", buf[buf_top-1], test_text[text_head]);
                 buf_top--;
-            }
-            else if(result == 2){
+                break;
+            case 1:
+                text_head++; 
+                break;
+            case 2:
                 printf("\r\nCongratulations, you are a capable typist\r\n");
                 return;
+            case 3:
+                text_head--;
+                buf_top--;
+                break;
+            default:
+                break;
             }
-            else if(result == 3){
-                buf[--buf_top] = '\0';
-            }
+            // Step 3.
+            // Step 3.1.
+            printf("\n%s\r\n", test_text);
+            // Step 3.2.
+            empty_text[text_head] = '^';
+            printf("%s\r", empty_text);
+            empty_text[text_head] = ' ';
         }
     }
 }
